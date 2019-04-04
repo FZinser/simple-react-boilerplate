03dcb4b3e265cbc4502eadd93d3b445f3ae6df9e
Javiani <eduardo.ottaviani@gmail.com>
2019-04-04 14:37:11 -0300

    Updating actions

--- src/Stores/index.js
+++ src/Stores/index.js
@@ -1,15 +1,10 @@
 // @Store
 export const initialState = {
     name : 'Zinser',
-    loading: false,
     things: []
 }
 
 export const actions = {
-
-    READY :( state, payload ) => ({
-        loading :false 
-    }),
 
     ADD_THING: (state, payload) =>({
         things: [...state.things, payload]
